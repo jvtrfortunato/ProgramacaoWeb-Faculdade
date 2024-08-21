82781c01f0d224ccf286ce3806e1a467e59fc5a8
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2024-08-20 23:24:04 -0300

    lista 2 - ex 1

--- Lista 2/Ex1/ex1.php
+++ Lista 2/Ex1/ex1.php
@@ -11,18 +11,51 @@ class Retangulo {
         $this->altura = 1;
     }
 
-    //Métodos
-    public function calcularPerimetro($base, $altura) {
-        
+    //Get e Set
+    public function getLargura() {
+        return $this->largura;
     }
 
-    public function calcularArea($base, $altura) {
+    public function getAltura() {
+        return $this->altura;
+    }
 
+    public function setLargura($largura) {
+        if ($largura > 0)
+            $this->largura = $largura;
+        else echo "ERRO: valor inválido para a largura!";
+    }
+
+    public function setAltura($altura) {
+        if ($altura > 0)
+            $this->altura = $altura;
+        else echo "ERRO: valor inválido para a altura!";
+    }
+
+    //Métodos
+    public function calcularPerimetro() {
+        $perimetro = (2 * $this->largura) + (2 * $this->altura);
+        return $perimetro;
+    }
+
+    public function calcularArea() {
+        $area = $this->largura * $this->altura;
+        return $area;
+    }
+
+    public function ehQuadrado() {
+        if ($this->largura == $this->altura)
+            echo "É quadrado!";
+        else echo "É retângulo!";
     }
 }
 
-
-
-
+$r1 = new Retangulo();
+$r1->setLargura(15);
+echo $r1->setAltura(-4)."<br>";
+$r1->setAltura(7);
+echo $r1->calcularPerimetro()."<br>";
+echo $r1->calcularArea()."<br>";
+echo $r1->ehQuadrado()."<br>";
 
 ?>
